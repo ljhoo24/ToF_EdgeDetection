68a4fc5c67ea2902eac58aab30e59a43a57934db
bachusf <ljhoo42@gmail.com>
2021-07-24 14:19:05 +0900

    initial viewer update

--- KinectDenoise/KinectDenoise/main.cpp
+++ KinectDenoise/KinectDenoise/main.cpp
@@ -5,8 +5,39 @@
 #include "DataManagement\ObjManager.h"
 #include "Util\timer.h"
 
+// azure kinect device
+unique_ptr<azure_kinect> kinectDevice;
+
+// showing point cloud (xyz with rgb)
+pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud;
+
+// scene draw part
+void drawScene(pcl::visualization::PCLVisualizer& viewer)
+{
+	// update every frame
+	auto ptc = kinectDevice->getPointCloudFromSingleDevice();
+}
+
 int main()
 {
-	
+	// azure kinect setup
+	kinectDevice = make_unique<azure_kinect>(1);
+
+	// pcl viewer setup
+	pcl::visualization::CloudViewer viewer("Tester");
+
+	//viewer.showCloud(cloud);
+
+	//This will only get called once
+	viewer.runOnVisualizationThreadOnce(once);
+
+	//This will get called once per visualization iteration
+	viewer.runOnVisualizationThread(drawScene);
+
+	while (!viewer.wasStopped())
+	{
+	}
+
+
 	return 0;
 }
