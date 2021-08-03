45e6d0cbc2528ff0603932eee58c4ddcab604547
JHYI <ljhoo24@naver.com>
2021-08-03 20:24:04 +0900

    FeatureDetect class added

--- KinectDenoise/KinectDenoise/main.cpp
+++ KinectDenoise/KinectDenoise/main.cpp
@@ -7,12 +7,6 @@
 #include "DataManagement\SWTest.h"
 #include "Util\timer.h"
 
-// azure kinect device
-unique_ptr<azure_kinect> kinectDevice;
-
-bool algFlag = false;
-bool updateFlag = true;
-bool saveFlag = false;
 
 // scene draw for realtime kinect
 void drawSceneKinect(pcl::visualization::PCLVisualizer& viewer)
@@ -70,36 +64,20 @@ void drawSceneKinect(pcl::visualization::PCLVisualizer& viewer)
 		viewer.updatePointCloud(pclPTC);
 }
 
-// scene draw for load obj
-void drawSceneObj(pcl::visualization::PCLVisualizer& viewer)
-{
-}
-
-void keyboardEventOccurred(const pcl::visualization::KeyboardEvent& event, void* viewer_void)
-{
-	if(event.getKeySym() == "a" && event.keyDown())
-		algFlag = !algFlag;
-	if (event.getKeySym() == "s" && event.keyDown())
-		updateFlag = !updateFlag;
-	if (event.getKeySym() == "d" && event.keyDown())
-		saveFlag = true;
-}
-
 /// todo : keyboard controller update
 int main()
 {
+	// azure kinect device
+	unique_ptr<azure_kinect> kinectDevice;
+
 	int menu = 0;
 
 	cout << "Choose Menu" << endl;
-	cout << "1 - Realtime Kinect" << endl;
+	cout << "1 - Kinect Single Shot" << endl;
 	cout << "2 - Load Obj" << endl;
 	cout << "else - exit" << endl << "input : ";
 
 	cin >> menu;
-
-	// pcl viewer setup
-	pcl::visualization::CloudViewer viewer("Tester");
-	viewer.registerKeyboardCallback(keyboardEventOccurred);
 
 	switch (menu)
 	{
@@ -108,37 +86,21 @@ int main()
 		// azure kinect setup
 		kinectDevice = make_unique<azure_kinect>(1);
 
-		//This will only get called once
-		//viewer.runOnVisualizationThreadOnce(once);
-
-		//This will get called once per visualization iteration
-		viewer.runOnVisualizationThread(drawSceneKinect);
 	}
 		break;
 	case 2:
 	{
-		ObjManager objMng;
+		/*ObjManager objMng;
 		objMng.ReadOBJ(".\\Data\\data.obj");
 
 		auto data = objMng.getOBJ();
 
-		auto pclPC = data_mng::ConvertVertex2PCL(data);
+		auto pclPC = data_mng::ConvertVertex2PCL(data);*/
 
-		//viewer.showCloud(pclPC);
-		//This will get called once per visualization iteration
-		viewer.runOnVisualizationThread(drawSceneObj);
 	}
 		break;
 	default:
 		break;
-	}
-
-	if (menu != 3)
-	{
-
-		while (!viewer.wasStopped())
-		{
-		}
 	}
 
 	return 0;
